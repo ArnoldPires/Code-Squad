ba2a50fd5fae9d20c138fb70b418702a9dc9bf63
Arnold <arnold.pires92@gmail.com>
2019-03-19 20:39:51 -0400

    finished project

---
+++ codesquad/js/js-code/rhyme-Generator/main.js
@@ -0,0 +1,19 @@
+let rhyme = "";
+function maryHad() {
+  rhyme += "Mary had a ";
+}
+function littleLamb() {
+  rhyme += "little lamb, ";
+}
+function fleece() {
+  rhyme += "whose fleece was white as snow.";
+}
+function lineBreak() {
+  rhyme += "\n";
+
+function displayrhyme() {
+  alert(rhyme += "");
+}
+function clearAll() {
+  rhyme = "";
+}
