faf5a11d9d9b713eaadbf71f3ef81c53799d8df4
Arnold <arnold.pires92@gmail.com>
2019-04-03 15:34:55 -0400

    stuff

--- codesquad/js/js-code/WePaint/main.js
+++ codesquad/js/js-code/WePaint/main.js
@@ -13,3 +13,19 @@ let premiumPaint = prompt('Do you want premium paint? Type yes or no (Comes as a
     paintCost = 20;
   }
 alert('The final price for painting your wall is $'+ paintCost + taxCost + cansOfPaint + sqaureFeet);
+
+let paintCost = 15
+let taxCost = 1.5
+let cansOfPaint = 1/175
+let firstName = prompt('Enter your first name');
+let lastName = prompt('Enter your last name');
+let streetAddress = prompt('Enter your street address');
+let city = prompt('Enter city name');
+let width = prompt('Enter width of wall to be painted');
+let height = prompt('Enter height of wall to be painted');
+let sqaureFeet = width * height
+let premiumPaint = prompt('Do you want premium paint? Type yes or no (Comes as an extra cost)');
+  if (premiumPaint === 'yes') {
+    paintCost = 20;
+  }
+alert('The final price for painting your wall is $'+ paintCost + taxCost + cansOfPaint + sqaureFeet);
