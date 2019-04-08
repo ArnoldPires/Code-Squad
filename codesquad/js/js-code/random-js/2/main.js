1e4e16159ef6c9b28b4cae716f33ecc371557f9e
ArnoldPires <arnold.pires92@gmail.com>
2019-04-07 22:55:21 -0400

    practice problems

--- codesquad/js/js-code/random-js/2/main.js
+++ codesquad/js/js-code/random-js/2/main.js
@@ -1,9 +1,21 @@
-//function square(x) {
-//  return x * x;
-//}
-//let y1 = square(2);
-//let y2 = square(5);
-//console.log(y1);
-//console.log(y2);
-
-function ownedPets
+let donation = prompt('How much would you like to donate?');
+let donateCar = prompt('Do you have an old car you would like to donate?');
+  if ((donation >= 1000 && donateCar === 'yes')){
+    alert('Thank you so much. You are now a platinum donor.')
+  }else if ((donation <=1000 && donateCar === 'yes')){
+    alert('Thank you so much. You are now a platinum donor.')
+  }else if ((donation >=1000 && donateCar === 'no')){
+    alert('Thank you so much. You are now a platinum donor.')
+  }else if ((donation <=0 && donateCar === 'no')){
+    alert('Maybe next time.')
+  }else if ((donation <=0 && donateCar === 'yes')){
+    alert('Thank you so much. You are now a platinum donor.')
+  }else if ((donation <=1000 && donateCar === 'no')){
+    alert('Thank you so much.')
+  }
+//You have a program that asks people to donate to your important cause.
+//The program should ask how much money (in dollars) they would like to donate.
+// It should then ask if they would like to donate their used car.
+//If the donation in dollars is greater than $1000 or if they say "yes" to donating their used car, then the program should tell them using the alert method "Thank you so much.
+//You are now a platinum donor."
+//Any other donation the program should just tell them "Thank you so much."  If they decide not to donate ($0 and no car), it should say "Maybe next time."
