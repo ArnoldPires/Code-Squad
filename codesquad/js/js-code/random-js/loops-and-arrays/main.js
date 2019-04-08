82e8420d7693c9d5dd9bd51b16da10f93630286e
Arnold <arnold.pires92@gmail.com>
2019-04-07 21:08:58 -0400

    stuff

---
+++ codesquad/js/js-code/random-js/loops-and-arrays/main.js
@@ -0,0 +1,8 @@
+// a loop that counts to 9. This includes the 0
+for ( i = 0; i < 10; i++)
+{
+  console.log(i);
+}
+//Typing in alert(numbers.length); Will tell you how many objects are in the array
+let numbers = ["1", "2", "3"];
+alert(numbers.length);
