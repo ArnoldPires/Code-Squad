3ed3f83aea44fbc0c6a51539608930c091006fe1
Arnold <arnold.pires92@gmail.com>
2019-04-22 13:22:41 -0400

    stuff

---
+++ codesquad/js/js-code/impostor-syndrome-coach/main.js
@@ -0,0 +1,9 @@
+let firstName = prompt('First Name'); {
+  if (firstName === firstName) {
+    alert('Hi ' + firstName);
+  }
+}
+let questionOne = prompt('Do you feel like other people always understand new coding concepts faster or better than you? (Yes/No)');
+let questionTwo = prompt('Are you smart enough to be a developer? (yes/no)');
+let questionThree = prompt("Do other people think you're a bad coder? (yes/no)");
+let questionFour = prompt('Has your code failed? (yes/no)');
