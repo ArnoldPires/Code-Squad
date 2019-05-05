de5d2b3ba701d6defd6a8bba0246e94a95b4a92d
ArnoldPires <arnold.pires92@gmail.com>
2019-05-05 19:12:15 -0400

    finished assignment

--- codesquad/js/js-code/broken-calculator/main.js
+++ codesquad/js/js-code/broken-calculator/main.js
@@ -3,32 +3,32 @@ let val;
 function displayVal() {
   document.getElementById("display").value = val;
 }
-function memRemember(val){
+function memRemember(val) {
 	// display value from memory
 	val = memory;
 	document.getElementById("display").value = val;
 }
-function replace(val){
+function replace(val) {
 	// replace value in the display window with 'val' argument
 	document.getElementById("display").value = val;
 }
-function append(val){
+function append(val) {
 	// append new value onto existing display
 	document.getElementById("display").value += val;
 }
-function memPlus(val){
+function memPlus(val) {
 	document.getElementById("display").value = parseFloat(memory) + parseFloat(val);
 }
-function memSave(val){
+function memSave(val) {
 	memory = document.getElementById("display").value;
 }
-function equals(){
+function equals() {
 	try{ 		// test the statement to catch errors (www.w3schools.com/js/js_errors.asp)
 		replace(
 			eval(document.getElementById("display").value)  // solve the math expression
 		)
 	}
-	catch(err){
+	catch(err) {
 		replace('Error') // display custom text if error occurs
 	}
 }
