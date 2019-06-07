f9e405d416bba399280e28449eaa6862e25ab003
ArnoldPires <arnold.pires92@gmail.com>
2019-06-07 19:03:35 -0400

    finished assignment

--- codesquad/projects/scorekeeper/js/main.js
+++ codesquad/projects/scorekeeper/js/main.js
@@ -1,3 +1,26 @@
-let p1button = document.getElementById('p1btn');
-let p2button = document.getElementById('p2btn');
-let reset = document.getElementById('reset');
+var totalScore1 = 0;
+var totalScore2 = 0
+var totalScore = 0;
+let p1button = document.querySelector("#playerOne").onclick = add1;
+let p2button = document.querySelector("#playerTwo").onclick = add2;
+let startOver = document.querySelector("#reset").onclick = zero;
+
+function add1() {
+  totalScore = totalScore1 +=1;
+  document.getElementById('playerOneScore').innerHTML = totalScore1;
+    if (totalScore1 === 5) {
+      alert("Player one Wins!")
+      document.getElementById("playerOneScore").style.color = "#9eff98";
+    }
+};
+function add2() {
+  totalScore = totalScore2 +=1;
+  document.getElementById('playerTwoScore').innerHTML = totalScore2;
+   if (totalScore2 === 5) {
+     alert("Player Two Wins!"),
+     document.getElementById("playerTwoScore").style.color = "#9eff98";
+   };
+};
+function zero() {
+  window.location.reload();
+};
